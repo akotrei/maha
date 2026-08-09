b7ac7a7739f2b182da901b09ce7ac3d30357ac74
kotan <akotrei@gmail.com>
2026-08-09 22:55:51 +0300

    wip

--- src/connection.h
+++ src/connection.h
@@ -1,7 +1,6 @@
 #ifndef CONNECTION_H
 #define CONNECTION_H
 
-
 typedef enum {
     CONN_TYPE_NETWORK_LISTEN,
     CONN_TYPE_IPC_LISTEN,
@@ -11,9 +10,28 @@ typedef enum {
 
 typedef struct {
     int fd;
-    conn_type_t type;
-    int slot_id;
+    union {
+        struct {
+            conn_type_t type;
+            int slot_id;
+        } active;
+        int next_free_idx;
+    } state;
 } conn_context_t;
+
+typedef struct {
+    conn_context_t *slots;
+    int head_free_idx;
+    int size;
+} registry_t;
+
+registry_t* registry_create(int size);
+
+void registry_destroy(registry_t *reg);
+
+conn_context_t* registry_pop_free(registry_t *reg, int fd, conn_type_t type);
+
+void registry_push_free(registry_t *reg, conn_context_t *slot);
 
 int conn_set_nonblocking(int fd);
 
--- src/server.c
+++ src/server.c
@@ -19,15 +19,14 @@ int main(int argc, char** argv) {
     parse_arguments(argc, argv, &config);
     printf("[C-SERVER] Initialisation is successful!\n");
 
-    int max_connections = config.max_clients + config.max_workers + 2;
-    conn_context_t *registry = calloc(max_connections, sizeof(conn_context_t));
+    int max_clients = config.max_clients;
+    int max_workers = config.max_workers;
+
+    int max_connections = max_clients + max_workers + 2;
+    registry_t *registry = registry_create(max_connections);
     if (!registry) {
         printf("[C-ERROR] Critical: Failed to allocate Memory Pool for connections.\n");
         return 1;
-    }
-    for (int i = 0; i < max_connections; i++) {
-        registry[i].fd = -1;
-        registry[i].slot_id = -1;
     }
     printf("[C-SERVER] Memory Pool for %d connections successfully allocated.\n", max_connections);
 
@@ -36,17 +35,12 @@ int main(int argc, char** argv) {
     int epoll_fd = epoll_manager_init(&config, &ipc_fd, &net_fd);
     if (epoll_fd == -1) {
         printf("[C-ERROR] Server initialization failed. Exiting.\n");
-        free(registry);
+        registry_destroy(registry);
         return 1;
     }
 
-    conn_context_t *ipc_listen_ctx = &registry[0];
-    ipc_listen_ctx->fd = ipc_fd;
-    ipc_listen_ctx->type = CONN_TYPE_IPC_LISTEN;
-
-    conn_context_t *net_listen_ctx = &registry[1];
-    net_listen_ctx->fd = net_fd;
-    net_listen_ctx->type = CONN_TYPE_NETWORK_LISTEN;
+    conn_context_t *ipc_listen_ctx = registry_pop_free(registry, ipc_fd, CONN_TYPE_IPC_LISTEN);
+    conn_context_t *net_listen_ctx = registry_pop_free(registry, net_fd, CONN_TYPE_NETWORK_LISTEN);
 
     // Bind to epoll
     struct epoll_event ev;
@@ -62,6 +56,12 @@ int main(int argc, char** argv) {
     printf("[C-SERVER] Server is ready. Both IPC and Network sockets are up.\n");
     printf("[C-SERVER] Entering main asynchronous loop...\n");
 
+    int current_clients = 0;
+    int current_workers = 0;
+    conn_context_t *ctx;
+    conn_context_t *free_slot;
+    int registry_idx;
+    struct epoll_event client_ev;
 
     // ==========================================
     // MAIN ASYNCHRONIOUS SERVER LOOP
@@ -84,23 +84,116 @@ int main(int argc, char** argv) {
         }
 
         for (int i = 0; i < num_events; i++) {
-            conn_context_t *ctx = (conn_context_t *)events[i].data.ptr;
-            printf("[C-SERVER] New event\n");
-            if (!ctx) continue;
+            ctx = (conn_context_t *)events[i].data.ptr;
+            if (!ctx || ctx->fd == -1) continue;
 
-            // Сюда мы шаг за шагом встроим обработку switch(ctx->type)
+            // Вычисляем индекс элемента в пуле памяти с помощью вашей математики указателей!
+            registry_idx = ctx - registry->slots;
+            
+            switch (ctx->state.active.type){
+                case CONN_TYPE_NETWORK_LISTEN: {
+                    // From net_fd
+                    while (1) {
+                        // 1. Check our limits
+                        if (current_clients >= max_clients) {
+                            printf("[C-WARNING] Client limit reached (%d). Rejecting incoming connection.\n", max_clients);
+                            int temp_fd = accept(ctx->fd, NULL, NULL);
+                            if (temp_fd >= 0) close(temp_fd); // Close politely
+                            break; 
+                        }
+
+                        // 2. Accept a new client
+                        int client_fd = accept(ctx->fd, NULL, NULL);
+                        if (client_fd < 0) {
+                            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                                break; // All clients are pulled out from the epoll queue
+                            }
+                            perror("[C-ERROR] accept failed for network client");
+                            break;
+                        }
+
+                        // 3. Make new sockets client non-blocking
+                        conn_set_nonblocking(client_fd);
+
+                        // 4. Search a new slot
+                        conn_context_t *free_slot = registry_pop_free(registry, client_fd, CONN_TYPE_CLIENT);
+                        if (!free_slot) {
+                            printf("[C-ERROR] Critical: No free memory slot found, but limit wasn't reached.\n");
+                            close(client_fd);
+                            break;
+                        }
+
+                        // 5. Bind to epoll
+                        client_ev.events = EPOLLIN | EPOLLET;
+                        client_ev.data.ptr = free_slot;
+                        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &client_ev) == -1) {
+                            perror("[C-ERROR] epoll_ctl: failed to add client");
+                            close(client_fd);
+                            registry_push_free(registry, free_slot);
+                            break;
+                        }
+
+                        current_clients++;
+                        printf("[C-SERVER] Client successfully registered on fd %d; number of clients\n", client_fd, current_clients);
+                    }
+                    break;
+                }
+
+                case CONN_TYPE_IPC_LISTEN: {
+                    // from IPC
+                    while (1) {
+                        // 1. Check our limits
+                        if (current_workers >= max_workers) {
+                            printf("[C-WARNING] Worker limit reached (%d). Rejecting new worker.\n", config.max_workers);
+                            int temp_fd = accept(ctx->fd, NULL, NULL);
+                            if (temp_fd >= 0) close(temp_fd); // Close politely
+                            break;
+                        }
+
+                        // 2. Accept a new worker
+                        int worker_fd = accept(ctx->fd, NULL, NULL);
+                        if (worker_fd < 0) {
+                            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                                break; // All workers are accepted
+                            }
+                            perror("[C-ERROR] accept failed for Python worker");
+                            break;
+                        }
+
+                        // 3. Make it non-blocking
+                        conn_set_nonblocking(worker_fd);
+
+                        // 4. Search a new slot
+                        conn_context_t *free_slot = registry_pop_free(registry, worker_fd, CONN_TYPE_WORKER);
+                        if (!free_slot) {
+                            printf("[C-ERROR] Critical: No free memory slot found, but limit wasn't reached.\n");
+                            close(worker_fd);
+                            break;
+                        }
+
+                        // 5. Bind to epoll
+                        client_ev.events = EPOLLIN | EPOLLET;
+                        client_ev.data.ptr = free_slot;
+                        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, worker_fd, &client_ev) == -1) {
+                            perror("[C-ERROR] epoll_ctl: failed to add client");
+                            close(worker_fd);
+                            registry_push_free(registry, free_slot);
+                            break;
+                        }
+
+                        current_workers++;
+                        printf("[C-SERVER] Python worker successfully registered on fd %d number of workers: %d\n", worker_fd, current_workers);
+                    }
+                    break;
+                }
+            }
+
         }
     }
 
     printf("[C-SERVER] Shutting down and cleaning memory pool...\n");
     
-    for (int i = 0; i < max_connections; i++) {
-        if (registry[i].fd != -1) {
-            close(registry[i].fd);
-        }
-    }
-    
-    free(registry);
+    registry_destroy(registry);
     close(epoll_fd);
     unlink(config.socket_path);
     
